cd05ee898528d3c784c1c37270fb741630c19bda
IdlKilic <idlkklc@gmail.com>
2025-04-18 23:02:00 +0300

    dataset araştırıldı ve yüklendi

--- simple_webcam_gui.py
+++ simple_webcam_gui.py
@@ -8,7 +8,7 @@ from PyQt5.QtGui import QImage, QPixmap
 
 class WebcamApp(QMainWindow):
     def __init__(self):
-        super().__init__()
+        super().__init__() 
         
         # Ana pencere ayarları
         self.setWindowTitle("Webcam Yüz Yaşlandırma")
